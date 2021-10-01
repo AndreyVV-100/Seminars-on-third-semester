d7949f965e98f342de698d9e0bd3c3c832d52408
AndreyVV-100 <viazovtsev.av@phystech.edu>
2021-10-01 10:54:07 +0300

    Add Sem5: realization of >.

---
+++ Sem5/main.c
@@ -0,0 +1,32 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
+int main (int argc, char** argv)
+{
+    if (argc < 3)
+    {
+        fprintf (stderr, "Error: too few parameters.\n");
+        return 0;
+    }
+
+    int new_stdout = -1;
+    if ((new_stdout = open (argv[1], O_WRONLY | O_CREAT | O_TRUNC, 0666)) == -1)
+    {
+        perror ("Can't open stdout");
+        return 0;
+    }
+
+    if (!dup2 (new_stdout, STDOUT_FILENO))
+    {
+        perror ("");
+        return 0;
+    }
+
+    close (new_stdout);
+    execvp (argv[2], argv + 2);
+    return 0;
+}
